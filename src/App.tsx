1eb7076fd5c8a8a16a4d38946288a9776bafb48b
dnrsm <daikinarushima@gmail.com>
2022-05-04 07:08:32 +0900

    feat: counter

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import styles from "./App.module.css";
 
 const IndexPage = lazy(() => import("./pages/Index"));
 const CatsPage = lazy(() => import("./pages/Cats"));
+const CounterPage = lazy(() => import("./pages/Counter"));
 
 const App: Component = () => {
   return (
@@ -13,10 +14,12 @@ const App: Component = () => {
         <div class={styles.nav}>
           <Link href="/">Todo Page</Link>
           <Link href="/cats">Cats Page </Link>
+          <Link href="/counter">Counter Page </Link>
         </div>
         <Routes>
           <Route path="/" element={<IndexPage />} />
           <Route path="/cats" element={<CatsPage />} />
+          <Route path="/counter" element={<CounterPage />} />
         </Routes>
       </div>
     </Router>
